8d78f41162ff723fd62518a90ec74b43b8dc4522
Jordan Olsen <2001jordanolsen@gmail.com>
2025-03-20 17:02:27 -0600

    created routes

---
+++ app/jobs/[name]/page.tsx
@@ -0,0 +1,8 @@
+export default function Job() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Job</h1>
+      <p className="mt-4">This is a job.</p>
+    </div>
+  );
+}
---
+++ app/jobs/page.tsx
@@ -0,0 +1,8 @@
+export default function Jobs() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Jobs</h1>
+      <p className="mt-4">This is the jobs page.</p>
+    </div>
+  );
+}
